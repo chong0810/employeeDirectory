8f77ad4720c29037d5b3f75e029225c23b504716
Chang Yeon Hong <hongchangyeon@gmail.com>
2020-09-03 16:44:09 -0500

    changes in the code to test heroku deployment

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Employee Directory link', () => {
     const { getByText } = render(<App />);
     const linkElement = getByText(/𝐄𝐦𝐩𝐥𝐨𝐲𝐞𝐞 𝐃𝐢𝐫𝐞𝐜𝐭𝐨𝐫𝐲/i);
     expect(linkElement).toBeInTheDocument();
